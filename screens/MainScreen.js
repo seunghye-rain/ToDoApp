102eb94ebbe3fec8634c1c37fbed93c91dbd4b97
bhyerain <seunghye0307@naver.com>
2025-04-06 01:51:30 +0900

    [Feat] ToDoApp 생성

---
+++ screens/MainScreen.js
@@ -0,0 +1,61 @@
+import { SafeAreaView, StyleSheet, Text, View, Platform } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import InputForm from "../components/InputForm";
+
+const MainScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={"default"} />
+      <Text style={styles.pageTitle}>ToDo App</Text>
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>할 일</Text>
+      </View>
+      <View style={styles.seperator} />
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>완료된 일</Text>
+      </View>
+      <InputForm />
+    </SafeAreaView>
+  );
+};
+
+export default MainScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? 50 : 0, //안드로이드일 때는 padding 20, OS일 때는 0 => android, ios 분기 처리 (ios의 경우 이미 safeAreaView 컴포넌트로 처리함)
+    backgroundColor: "#f7f8fa",
+  },
+  pageTitle: {
+    marginBottom: 10,
+    paddingHorizontal: 15,
+    fontSize: 50,
+    fontWeight: "600",
+  },
+  seperator: {
+    marginHorizontal: 10,
+    marginTop: 25,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "rgba(0, 0, 0, 0.2)",
+  },
+  listView: {
+    flex: 1,
+  },
+  listTitle: {
+    marginBottom: 25,
+    paddingHorizontal: 15,
+    fontSize: 30,
+    fontWeight: "500",
+  },
+  emptyListText: {
+    paddingTop: 10,
+    paddingBottom: 15,
+    paddingHorizontal: 15,
+    fontSize: 15,
+    lineHeight: 20,
+    color: "#737373",
+  },
+});
